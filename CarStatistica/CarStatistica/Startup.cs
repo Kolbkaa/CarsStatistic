06b98d60ec228f827fc9318cb1c6cb46c6a8955e
Dawid Kolbusz <dawid.kolbusz1@gmail.com>
2020-02-24 22:19:30 +0100

    Add Error Page

--- CarStatistica/CarStatistica/Startup.cs
+++ CarStatistica/CarStatistica/Startup.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using CarStatistica.Data;
 using CarStatistica.Data.Repositories;
 using CarStatistica.Models;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
@@ -31,7 +33,7 @@ namespace CarStatistica
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<AppDbContext>(options =>
-                options.UseSqlServer(_configuration.GetConnectionString("SqlExpress")).UseLoggerFactory(MyLoggerFactory)
+                options.UseSqlServer(_configuration.GetConnectionString("WebioSqlExpress")).UseLoggerFactory(MyLoggerFactory)
             );
 
             services.AddDefaultIdentity<User>().AddEntityFrameworkStores<AppDbContext>();
@@ -62,6 +64,37 @@ namespace CarStatistica
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler(errorApp =>
+                {
+                    errorApp.Run(async context =>
+                    {
+                        context.Response.StatusCode = 500;
+                        context.Response.ContentType = "text/html";
+
+                        await context.Response.WriteAsync("<html lang=\"en\"><body>\r\n");
+                        await context.Response.WriteAsync("ERROR!<br><br>\r\n");
+
+                        var exceptionHandlerPathFeature =
+                            context.Features.Get<IExceptionHandlerPathFeature>();
+
+                        // Use exceptionHandlerPathFeature to process the exception (for example, 
+                        // logging), but do NOT expose sensitive error information directly to 
+                        // the client.
+
+                        if (exceptionHandlerPathFeature?.Error is FileNotFoundException)
+                        {
+                            await context.Response.WriteAsync("File error thrown!<br><br>\r\n");
+                        }
+
+                        await context.Response.WriteAsync("<a href=\"/\">Home</a><br>\r\n");
+                        await context.Response.WriteAsync("</body></html>\r\n");
+                        await context.Response.WriteAsync(new string(' ', 512)); // IE padding
+                    });
+                });
+                app.UseHsts();
             }
             app.UseHttpsRedirection();
             app.UseRouting();
